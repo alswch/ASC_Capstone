0d0d85b5a9331018cb6343144ba1f15b00ac5acf
alswch <alswchung@gmail.com>
2017-06-16 17:49:41 -0400

    updated function that user guess letter and submit. check if the guessed letter is correct

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
   console.log("jQuery ready");
 
   var WheelofFortune = {
+    wordArray: [{word:"water", hint:"Major source of life"},
+                {word:"coffee", hint:"Morning booster"},
+                {word:"airplane", hint:"Travel fast"},
+                {word:"laptop", hint:"A carrying device"},
+                {word:"sandwich", hint:"Lunch"} ],
+    currentWord: null,
+    totalPoints: 0,
 
     initialize: function() {
       console.log("==initialize==");
@@ -10,18 +17,19 @@ $(document).ready(function() {
     },
 
     // ===== ===== ===== WORD BOX DISPLAY ===== ===== =====
-    // empty array that contains words
-
     displayLetter: function() {
-      var words = "water";
-      for (var i = 0; i < words.length; i++) {
-      var nextLetter = words[i];
+      var randomNumber = Math.floor(Math.random()*5);
+      var phrase = WheelofFortune.wordArray[randomNumber].word;
+      this.currentWord = phrase;
+      console.log("currentWord", this.currentWord);
+      for (var i = 0; i < phrase.length; i++) {
+      var nextLetter = phrase[i];
         console.log(nextLetter);
         // var letterBox = $("<div/>", {id: 'letter_'+i, class: 'letterBox'});
         // var letter = $("<p/>", {class:'letter'});
         var letterBox = "<div id='letter_"+i+"'class='letterBox'><p class='letter'>" + nextLetter + "</p></div>"
         $('#gameboardContainer').append(letterBox);
-        // $('#gameboardContainer').children().last().html(nextLetter);
+        $('#gameboardContainer').children().last().html(nextLetter);
       };
     },
 
@@ -29,15 +37,21 @@ $(document).ready(function() {
       console.log("==guessNextLetter==");
       var self = this;
       $('#submitBtn').on('click', function(){
-        console.log("==submitBtn==");
-        var guessText = $('#guessText').val();
-        $('#guessTextDisplay').html(guessText);
-        // self.displayGuessText();
-      })
+        self.checkLetter();
+      });
     },
-    // displayGuessText: function() {
-    //   console.log("==displayGuessText==");
-    // }
+    checkLetter: function() {
+      var guessText = $('#guessText').val();
+      $('#guessTextDisplay').children().html(guessText);
+      for (var i = 0; i < this.currentWord.length; i++) {
+        var nextLetterCheck = this.currentWord[i];
+        console.log("nextLetterCheck:", nextLetterCheck);
+        if (guessText == nextLetterCheck) {
+          WheelofFortune.totalPoints = WheelofFortune.totalPoints + 100;
+          console.log(WheelofFortune.totalPoints);
+        }
+      }
+    }
 
   }
   WheelofFortune.initialize();
